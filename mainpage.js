32c04c9cc16cfd9a4c7047425ac12f09fb406e62
Klever <kgopar20@wooster.edu>
2020-11-02 20:34:53 -0500

    electron project with main page, order placed page

---
+++ mainpage.js
@@ -0,0 +1,72 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+//Listen for the app to be ready
+app.on('ready', function(){
+  //create new window
+  mainWindow = new BrowserWindow({});
+
+  //Load html into window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainpage.html'),
+    protocol:'file:',
+    slashes:true
+  }));
+  // Quit app when closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
+
+  //Build the menu from the tempelate
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  //Insert the menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// create menu template for the app
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu:[
+      {
+        label:'Quit App',
+        accelorator: process.platform == 'darwin' ? 'Command+Q' :
+        'ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({
+    label: null
+  });
+}
+
+//Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        label: 'Toggle DevTools',
+        accelorator: process.platform == 'darwin' ? 'Command+I' :
+        'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
